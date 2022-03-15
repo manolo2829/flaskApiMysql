2e7c09b19a0b14057c538349ac2359a2d52d2f29
unknown <manudiez2003@gmail.com>
2022-03-15 15:12:06 -0300

    inicio

---
+++ App.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def Index():
+    return 'Hello World'
+
+@app.route('/add_contact')
+def add_contact():
+    return 'agregar contacto'
+
+@app.route('/edit')
+def edit_contact():
+    return 'edit contact'
+
+@app.route('/delete')
+def delete_contact():
+    return 'delete contact'
+
+
+
+if __name__ == '__main__':
+    app.run(port = 3000, debug = True)
